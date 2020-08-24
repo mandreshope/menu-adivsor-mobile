fda4ac009c25841a9ee0f32594e79b950abe1deb
RANDRIANANDRAINA Hajanirina Ridjvan <darijavan@gmail.com>
2020-08-24 17:07:06 +0200

    ✨ restructure code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:menu_advisor/main.dart';
+import 'package:menu_advisor/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
