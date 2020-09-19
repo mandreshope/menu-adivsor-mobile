b991f5984a369d022f7b4daf6b59c240bf8bc998
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2020-09-20 02:03:05 +0300

    add other color constants

--- lib/src/constants/colors.dart
+++ lib/src/constants/colors.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 
-final Color CRIMSON = Color(0xffda143c);
-final Color BACKGROUND_COLOR = Color(0xfff5f5f5);
+const Color CRIMSON = Color(0xffda143c);
+const Color BACKGROUND_COLOR = Color(0xfff5f5f5);
+const Color TITLE_COLOR = Color(0xff2c4251);
+const Color TEAL = Color(0xff278181);
