e771afc7bd4e6f5b3519243eaa9bc475df934630
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2020-09-20 02:11:47 +0300

    create FadeAnimation

---
+++ lib/src/animations/FadeAnimation.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+import 'package:supercharged/supercharged.dart';
+
+enum FadeAnimationType { opacity, translateY }
+
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final Widget child;
+  final Curve curve;
+
+  FadeAnimation(
+    this.delay,
+    this.child, {
+    this.curve = Curves.linear,
+  });
+
+  final _tween = MultiTween<FadeAnimationType>()
+    ..add(FadeAnimationType.opacity, 0.0.tweenTo(1.0), 500.milliseconds)
+    ..add(FadeAnimationType.translateY, 30.0.tweenTo(0.0), 500.milliseconds);
+
+  @override
+  Widget build(BuildContext context) {
+    return PlayAnimation<MultiTweenValues<FadeAnimationType>>(
+      tween: _tween,
+      duration: _tween.duration,
+      delay: delay.seconds,
+      curve: curve,
+      child: child,
+      builder: (context, child, value) {
+        return Opacity(
+          opacity: value.get(FadeAnimationType.opacity),
+          child: Transform.translate(
+            offset: Offset(0.0, value.get(FadeAnimationType.translateY)),
+            child: child,
+          ),
+        );
+      },
+    );
+  }
+}
