42f9f54bf3bf05c9e0a676ff6a05887cc67739a0
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2020-09-25 15:41:32 +0300

    finish login screen

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -1,14 +1,18 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:menu_advisor/src/animations/FadeAnimation.dart';
 import 'package:menu_advisor/src/components/logo.dart';
 import 'package:menu_advisor/src/constants/colors.dart';
 import 'package:menu_advisor/src/pages/home.dart';
 import 'package:menu_advisor/src/pages/signup.dart';
-import 'package:menu_advisor/src/providers/RouteContext.dart';
+import 'package:menu_advisor/src/providers/AuthContext.dart';
 import 'package:menu_advisor/src/routes/routes.dart';
+import 'package:menu_advisor/src/utils/AppLocalization.dart';
 import 'package:menu_advisor/src/utils/routing.dart';
 import 'package:provider/provider.dart';
-import 'package:supercharged/supercharged.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -41,162 +45,183 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(100),
-                    bottomRight: Radius.circular(100),
+              // Top banner
+              FadeAnimation(
+                0.4,
+                Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(100),
+                      bottomRight: Radius.circular(100),
+                    ),
+                    color: Colors.white,
                   ),
-                  color: Colors.white,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    MenuAdvisorLogo(
-                      size: 70,
-                    ),
-                    MenuAdvisorTextLogo(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      color: CRIMSON,
-                      fontSize: 20,
-                    ),
-                  ],
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      MenuAdvisorLogo(
+                        size: 70,
+                      ),
+                      MenuAdvisorTextLogo(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        color: CRIMSON,
+                        fontSize: 20,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: 30,
-                  horizontal: 30,
+              // end top banner
+
+              // Form
+              FadeAnimation(
+                0.8,
+                Container(
+                  padding: EdgeInsets.all(30),
+                  width: MediaQuery.of(context).size.width - 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white70,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          AppLocalizations.of(context).translate("welcome"),
+                          style: TextStyle(
+                            fontFamily: 'Golden Ranger',
+                            color: CRIMSON,
+                            fontSize: 50,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      TextFormField(
+                        focusNode: _emailFocus,
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                        decoration: InputDecoration(
+                          labelText: AppLocalizations.of(context)
+                              .translate("mail_address_placeholder"),
+                        ),
+                        onFieldSubmitted: (_) {
+                          _emailFocus.unfocus();
+                          FocusScope.of(context).requestFocus(_passwordFocus);
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        focusNode: _passwordFocus,
+                        controller: _passwordController,
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        textInputAction: TextInputAction.done,
+                        decoration: InputDecoration(
+                          labelText: AppLocalizations.of(context)
+                              .translate("password_placeholder"),
+                        ),
+                        onFieldSubmitted: (_) => _submitForm(),
+                      ),
+                      SizedBox(height: 40),
+                      RaisedButton(
+                        padding: EdgeInsets.all(15),
+                        color: CRIMSON,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        onPressed: _submitForm,
+                        child: loading
+                            ? SizedBox(
+                                width: 20,
+                                height: 20,
+                                child: FittedBox(
+                                  child: CircularProgressIndicator(
+                                    valueColor: AlwaysStoppedAnimation<Color>(
+                                        Colors.white),
+                                  ),
+                                ),
+                              )
+                            : Text(
+                                AppLocalizations.of(context)
+                                    .translate("login_button"),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
+                      ),
+                      SizedBox(height: 10),
+                      FlatButton(
+                        child: Text(
+                          AppLocalizations.of(context)
+                              .translate("skip_for_now"),
+                        ),
+                        onPressed: () {
+                          RouteUtil.goTo(
+                            context: context,
+                            child: HomePage(),
+                            routeName: homeRoute,
+                            method: RoutingMethod.atTop,
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-                width: MediaQuery.of(context).size.width - 60,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white70,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Align(
-                      alignment: Alignment.center,
+              ),
+              // End form
+
+              // Create account button part
+              FadeAnimation(
+                0.8,
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 30,
+                  ),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      stops: [
+                        0,
+                        .6,
+                      ],
+                      colors: [
+                        Colors.white.withOpacity(0),
+                        Colors.white60,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Center(
+                    child: InkWell(
+                      onTap: () {
+                        RouteUtil.goTo(
+                          context: context,
+                          child: SignupPage(),
+                          routeName: signupRoute,
+                        );
+                      },
                       child: Text(
-                        "Bienvenu",
+                        AppLocalizations.of(context)
+                            .translate("create_account"),
                         style: TextStyle(
-                          fontFamily: 'Golden Ranger',
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
                           color: CRIMSON,
-                          fontSize: 50,
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    TextFormField(
-                      focusNode: _emailFocus,
-                      controller: _emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      textInputAction: TextInputAction.next,
-                      decoration: InputDecoration(
-                        labelText: "Votre adresse mail",
-                      ),
-                      onFieldSubmitted: (_) {
-                        _emailFocus.unfocus();
-                        FocusScope.of(context).requestFocus(_passwordFocus);
-                      },
-                    ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      focusNode: _passwordFocus,
-                      controller: _passwordController,
-                      keyboardType: TextInputType.text,
-                      obscureText: true,
-                      textInputAction: TextInputAction.done,
-                      decoration: InputDecoration(
-                        labelText: "Mot de passe",
-                      ),
-                      onFieldSubmitted: (_) => _submitForm(),
-                    ),
-                    SizedBox(height: 40),
-                    RaisedButton(
-                      padding: EdgeInsets.all(15),
-                      color: CRIMSON,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      onPressed: _submitForm,
-                      child: loading
-                          ? SizedBox(
-                            width: 20,
-                            height: 20,
-                            child: FittedBox(
-                                child: CircularProgressIndicator(
-                                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                                ),
-                              ),
-                          )
-                          : Text(
-                              "Connexion",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
-                            ),
-                    ),
-                    SizedBox(height: 10),
-                    FlatButton(
-                      child: Text("Passer pour le moment"),
-                      onPressed: () {
-                        print("Passer...");
-                        RouteUtil.goTo(
-                          context: context,
-                          child: HomePage(),
-                          routeName: homeRoute,
-                          method: RouteMethod.atTop,
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: 30,
-                ),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    stops: [
-                      0,
-                      .6,
-                    ],
-                    colors: [
-                      Colors.white.withOpacity(0),
-                      Colors.white60,
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
-                ),
-                child: Center(
-                  child: InkWell(
-                    onTap: () {
-                      RouteUtil.goTo(
-                        context: context,
-                        child: SignupPage(),
-                        routeName: signupRoute,
-                      );
-                    },
-                    child: Text(
-                      "Créer un compte",
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: CRIMSON,
-                      ),
-                    ),
                   ),
                 ),
               ),
+              // -------------------------
             ],
           ),
         ),
@@ -204,10 +229,39 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  _submitForm() {
-    print('Loging in...');
+  _submitForm() async {
+    final String email = _emailController.value.text,
+        password = _passwordController.value.text;
+
+    if (email.isEmpty || password.isEmpty) {
+      Fluttertoast.showToast(
+        msg: AppLocalizations.of(context).translate("blank_email_or_password"),
+        backgroundColor: CRIMSON,
+        textColor: Colors.white,
+      );
+      return;
+    }
+
     setState(() {
       loading = true;
     });
+
+    AuthContext authContext = Provider.of<AuthContext>(context, listen: false);
+    try {
+      final result = await authContext.login(email, password);
+      print(result.toString());
+    } catch (error) {
+      print(error);
+    } finally {
+      setState(() {
+        loading = false;
+      });
+      Fluttertoast.showToast(
+        msg:
+            AppLocalizations.of(context).translate("invalid_email_or_password"),
+        backgroundColor: CRIMSON,
+        textColor: Colors.white,
+      );
+    }
   }
 }
