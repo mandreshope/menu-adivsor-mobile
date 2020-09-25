8fb6d3e008855ec4cbb7b7fb63891be10b23196a
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2020-09-26 00:41:27 +0300

    add BRIGHT_RED color

--- lib/src/constants/colors.dart
+++ lib/src/constants/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color CRIMSON = Color(0xffda143c);
+const Color BRIGHT_RED = Color(0xfff26928);
 const Color BACKGROUND_COLOR = Color(0xfff5f5f5);
 const Color DARK_BLUE = Color(0xff2c4251);
 const Color TEAL = Color(0xff278181);
