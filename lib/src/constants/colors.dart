2b4f16ac9f04c47e4a2000a584fa96c6e5022426
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2020-09-25 15:32:23 +0300

    change typo

--- lib/src/constants/colors.dart
+++ lib/src/constants/colors.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 const Color CRIMSON = Color(0xffda143c);
 const Color BACKGROUND_COLOR = Color(0xfff5f5f5);
-const Color TITLE_COLOR = Color(0xff2c4251);
+const Color DARK_BLUE = Color(0xff2c4251);
 const Color TEAL = Color(0xff278181);
