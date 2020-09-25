08015d314a4794cfb699c9b291b5ba0a98733f12
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2020-09-25 15:32:50 +0300

    finish cards

--- lib/src/components/cards.dart
+++ lib/src/components/cards.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:menu_advisor/src/constants/colors.dart';
 import 'package:menu_advisor/src/models.dart';
+import 'package:menu_advisor/src/utils/AppLocalization.dart';
 
 class CategoryCard extends StatelessWidget {
   final String image;
@@ -42,14 +43,14 @@ class CategoryCard extends StatelessWidget {
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     border: Border.all(
-                      color: TITLE_COLOR,
+                      color: DARK_BLUE,
                       width: 2,
                     ),
                   ),
                   child: SvgPicture.asset(
                     image,
                     height: 40,
-                    color: TITLE_COLOR,
+                    color: DARK_BLUE,
                   ),
                 ),
                 SizedBox(
@@ -58,7 +59,7 @@ class CategoryCard extends StatelessWidget {
                 Text(
                   name,
                   style: TextStyle(
-                    color: TITLE_COLOR,
+                    color: DARK_BLUE,
                     fontFamily: 'Golden Ranger',
                     fontSize: 18,
                   ),
@@ -70,7 +71,7 @@ class CategoryCard extends StatelessWidget {
                   child: Container(
                     padding: const EdgeInsets.all(6),
                     decoration: BoxDecoration(
-                      color: TITLE_COLOR,
+                      color: DARK_BLUE,
                       shape: BoxShape.circle,
                     ),
                     child: Icon(
@@ -101,15 +102,138 @@ class FoodCard extends StatelessWidget {
     return Container(
       width: 300,
       child: AspectRatio(
-        aspectRatio: 1.7,
+        aspectRatio: 1.5,
         child: Card(
           elevation: 4.0,
           color: Colors.white,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(30),
           ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                bottom: 0,
+                left: 0,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      child: Row(
+                        children: [],
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 30.0,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Text(
+                              food.name,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              food.restaurant.name,
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              "${food.price}€",
+                              style: TextStyle(
+                                fontSize: 21,
+                                color: Colors.yellow[700],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        RawMaterialButton(
+                          fillColor: DARK_BLUE,
+                          padding: const EdgeInsets.all(15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(30),
+                              topRight: Radius.circular(30),
+                            ),
+                          ),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            child: Icon(
+                              Icons.add,
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        // _renderNotes(food.ratings)
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                right: 40,
+                top: 0,
+                bottom: 0,
+                child: food.imageURL != null
+                    ? Image.network(food.imageURL)
+                    : Icon(
+                        Icons.fastfood,
+                        size: 60,
+                      ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _renderNotes(double ratings) {
+    return Row(
+      children: [
+        for (int i = 0; i < 5; i++)
+          if (ratings - i > 0)
+            if ((ratings - i) % 1 < 1)
+              Icon(
+                Icons.star_half,
+                color: CRIMSON,
+                size: 15,
+              )
+            else
+              Icon(
+                Icons.star,
+                color: CRIMSON,
+                size: 15,
+              )
+          else
+            Icon(
+              Icons.star_border,
+              color: CRIMSON,
+              size: 15,
+            )
+      ],
     );
   }
 }
@@ -127,12 +251,53 @@ class RestaurantCard extends StatelessWidget {
     return Container(
       width: 300,
       child: AspectRatio(
-        aspectRatio: 1.7,
+        aspectRatio: 1.5,
         child: Card(
           elevation: 4.0,
           color: Colors.white,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(30),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Text(
+                          restaurant.name,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          AppLocalizations.of(context).translate(restaurant.type),
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Image.network(
+                  restaurant.imageURL,
+                  width: 120,
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
           ),
         ),
       ),
