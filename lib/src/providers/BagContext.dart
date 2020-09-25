f2c635b07a73ff2ea4d542bb0b2f8c0584ba9b6a
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2020-09-25 15:34:07 +0300

    create context for bag content

---
+++ lib/src/providers/BagContext.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:menu_advisor/src/models.dart';
+
+class BagContext extends ChangeNotifier {
+  Map<Food, int> _items;
+  int _itemCount = 0;
+
+  bool addItem(Food item, number) {
+    _itemCount++;
+    return false;
+  }
+
+  Map<Food, int> get items => _items;
+
+  int get itemCount => _itemCount;
+}
