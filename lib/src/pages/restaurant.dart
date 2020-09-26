cb608f3e38c173e1e755cce60d7236217640987b
RANDRIANANDRAINA Hajanirina Ridjvan <31767252+darijavan@users.noreply.github.com>
2020-09-26 14:23:11 +0300

    add restaurant param to RestaurantPage constructor

--- lib/src/pages/restaurant.dart
+++ lib/src/pages/restaurant.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:menu_advisor/src/models.dart';
 
 class RestaurantPage extends StatefulWidget {
+  final Restaurant restaurant;
+
+  const RestaurantPage({
+    Key key,
+    this.restaurant,
+  }) : super(key: key);
+
   @override
   _RestaurantPageState createState() => _RestaurantPageState();
 }
