1ee3a5ca9f8b8b9d42c5b8b4c1f06bce004acbb0
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2020-09-29 04:43:58 +0300

    change typo & add refresh_token

--- lib/src/services/api.dart
+++ lib/src/services/api.dart
@@ -6,16 +6,24 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Api {
   final String _apiURL = 'https://menu-advisor.herokuapp.com/api';
-  String _token;
+  String _accessToken;
+  String _refreshToken;
 
-  set token(String token) {
-    _token = token;
+  set accessToken(String accessToken) {
+    _accessToken = accessToken;
     SharedPreferences.getInstance().then((sharedPrefs) {
-      sharedPrefs.setString('token', token);
+      sharedPrefs.setString('access_token', accessToken);
     });
   }
 
-  String get token => _token;
+  set refreshToken(String refreshToken) {
+    _refreshToken = refreshToken;
+    SharedPreferences.getInstance().then((sharedPrefs) {
+      sharedPrefs.setString('refresh_token', refreshToken);
+    });
+  }
+
+  String get accessToken => _accessToken;
 
   Api._privateConstructor() {
     _checkExistingTokenCache();
@@ -23,8 +31,20 @@ class Api {
 
   _checkExistingTokenCache() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    if (prefs.containsKey('token')) _token = prefs.getString('token');
+    if (prefs.containsKey('access_token') &&
+        prefs.containsKey('refresh_token')) {
+      _accessToken = prefs.getString('access_token');
+      _refreshToken = prefs.getString('refresh_token');
+    }
+    // var newTokens = await _checkToken();
+    // if (newTokens != null)
   }
+
+  // Future _checkToken() {
+  //   return http.get('$_apiURL/check-token?access_token=$_accessToken&refresh_token=$_refreshToken').then((response) {
+  //     if (response.statusCode == 200) {}
+  //   });
+  // }
 
   static Api _instance;
 
@@ -40,7 +60,7 @@ class Api {
     ).then((response) {
       if (response.statusCode == 200) {
         Map<String, dynamic> data = json.decode(response.body);
-        token = data['access_token'];
+        accessToken = data['access_token'];
         User user = User.fromJson(data['user']);
         return user;
       }
@@ -125,7 +145,7 @@ class Api {
     return http.get(
       '$_apiURL/foods$query',
       headers: {
-        "authorization": "Bearer $_token",
+        "authorization": "Bearer $_accessToken",
       },
     ).then((response) {
       if (response.statusCode == 200) {
@@ -143,7 +163,7 @@ class Api {
     return http.post('$_apiURL/users/favoriteFood/add', body: {
       "id": food.id,
     }, headers: {
-      "authorization": "Bearer $_token",
+      "authorization": "Bearer $_accessToken",
     }).then((response) {
       if (response.statusCode == 200) {
         return true;
@@ -158,7 +178,7 @@ class Api {
     return http.post('$_apiURL/users/favoriteFood/delete', body: {
       "id": food.id,
     }, headers: {
-      "authorization": "Beare $_token",
+      "authorization": "Beare $_accessToken",
     }).then((response) {
       if (response.statusCode == 200) {
         return true;
