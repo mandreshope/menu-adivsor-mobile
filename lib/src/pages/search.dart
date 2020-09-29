70d0f7d2167047e77e924f35f250c485303cf7bc
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2020-09-29 16:30:05 +0300

    init SearchPage

--- lib/src/pages/search.dart
+++ lib/src/pages/search.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:menu_advisor/src/utils/AppLocalization.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -6,13 +8,78 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  bool isSettingExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Rechercher"),
       ),
-      body: Container(),
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned(
+              top: 10,
+              left: 20,
+              right: 20,
+              child: Container(
+                constraints: BoxConstraints(
+                  maxHeight: isSettingExpanded ? double.infinity : 50,
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(.8),
+                    borderRadius: BorderRadius.circular(50),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 10,
+                        spreadRadius: 0,
+                        color: Colors.black26,
+                      ),
+                    ]),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.search,
+                          size: 16,
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: TextFormField(
+                            decoration: InputDecoration.collapsed(
+                              hintText: AppLocalizations.of(context)
+                                  .translate("find_something"),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () {
+                            setState(() {
+                              isSettingExpanded = !isSettingExpanded;
+                            });
+                          },
+                          icon: FaIcon(
+                            FontAwesomeIcons.slidersH,
+                            size: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
