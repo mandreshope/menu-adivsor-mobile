033cad703d9b2fff145d1f08faa9cb76aced3609
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2020-09-30 15:19:09 +0300

    add signout button

--- lib/src/pages/profile.dart
+++ lib/src/pages/profile.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:menu_advisor/src/components/utilities.dart';
+import 'package:menu_advisor/src/pages/login.dart';
+import 'package:menu_advisor/src/providers/AuthContext.dart';
+import 'package:menu_advisor/src/providers/SettingContext.dart';
+import 'package:menu_advisor/src/routes/routes.dart';
 import 'package:menu_advisor/src/utils/AppLocalization.dart';
+import 'package:menu_advisor/src/utils/routing.dart';
+import 'package:provider/provider.dart';
 
-class ProfilePage extends StatelessWidget {
-  final String language = "Français";
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
   final List<String> _supportedLanguages = [
     'fr',
     'en',
@@ -15,76 +26,103 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScaffoldWithBottomMenu(
-      appBar: AppBar(
-        title: Text("Profil et paramètres"),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 250,
-            child: Center(
-              child: CircleAvatar(
-                radius: 60,
-                backgroundColor: Colors.white,
-                child: Icon(
-                  Icons.person,
-                  color: Colors.black,
-                  size: 50,
+    return Consumer<SettingContext>(
+      builder: (_, settingContext, __) {
+        String languageCode = settingContext.languageCode;
+
+        return ScaffoldWithBottomMenu(
+          appBar: AppBar(
+            title: Text(
+              'Profil et paramètres',
+            ),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                tooltip: AppLocalizations.of(context).translate('signout'),
+                onPressed: () async {
+                  await Provider.of<AuthContext>(context, listen: false)
+                      .logout();
+                  RouteUtil.goTo(
+                    context: context,
+                    child: LoginPage(),
+                    routeName: loginRoute,
+                    method: RoutingMethod.atTop,
+                  );
+                },
+                icon: FaIcon(
+                  FontAwesomeIcons.signOutAlt,
                 ),
               ),
-            ),
+            ],
           ),
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              color: Colors.white,
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    AppLocalizations.of(context).translate("my_settings"),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+          body: Column(
+            children: [
+              Container(
+                height: 250,
+                child: Center(
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.black,
+                      size: 50,
                     ),
                   ),
-                  SizedBox(height: 30),
-                  Text(
-                    AppLocalizations.of(context).translate("language"),
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  DropdownButton<String>(
-                      elevation: 16,
-                      onChanged: (String languageCode) {
-
-                      },
-                      style: TextStyle(
-                        color: Colors.grey[700],
-                      ),
-                      items: [
-                        for (int i = 0; i < _supportedLanguages.length; i++)
-                          DropdownMenuItem<String>(
-                            value: _supportedLanguages[i],
-                            child: Text(
-                              _languages[i],
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                          )
-                      ]),
-                  SizedBox(height: 30),
-                  Text(
-                    AppLocalizations.of(context).translate("my_payment_cards"),
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                ],
+                ),
               ),
-            ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        AppLocalizations.of(context).translate("my_settings"),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Text(
+                        AppLocalizations.of(context).translate("language"),
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      DropdownButton<String>(
+                          elevation: 16,
+                          isExpanded: true,
+                          value: languageCode,
+                          onChanged: (String languageCode) {},
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                          ),
+                          items: [
+                            for (int i = 0; i < _supportedLanguages.length; i++)
+                              DropdownMenuItem<String>(
+                                value: _supportedLanguages[i],
+                                child: Text(
+                                  _languages[i],
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                              )
+                          ]),
+                      SizedBox(height: 30),
+                      Text(
+                        AppLocalizations.of(context)
+                            .translate("my_payment_cards"),
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
