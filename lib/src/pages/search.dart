8416d470a0efeed831b6630e33f6d9374df66d1d
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2020-09-30 16:19:54 +0300

    update search page

--- lib/src/pages/search.dart
+++ lib/src/pages/search.dart
@@ -8,40 +8,67 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController _searchController = TextEditingController();
+  bool loading = false;
   bool isSettingExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _searchController.addListener(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text("Rechercher"),
       ),
       body: SafeArea(
         child: Stack(
           fit: StackFit.expand,
           children: [
+            SingleChildScrollView(
+              padding: EdgeInsets.only(top: 90),
+              child: _searchController.value.text.length == 0
+                  ? Center(
+                      child: Text(
+                        "Commencer par entrer votre recherche...",
+                      ),
+                    )
+                  : Column(
+                      children: [],
+                    ),
+            ),
             Positioned(
-              top: 10,
+              top: 20,
               left: 20,
               right: 20,
-              child: Container(
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 100),
                 constraints: BoxConstraints(
-                  maxHeight: isSettingExpanded ? double.infinity : 50,
+                  maxHeight: isSettingExpanded ? 200 : 50,
                 ),
                 padding: const EdgeInsets.symmetric(
                   horizontal: 20,
                 ),
                 decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(.8),
-                    borderRadius: BorderRadius.circular(50),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 10,
-                        spreadRadius: 0,
-                        color: Colors.black26,
-                      ),
-                    ]),
+                  color: Colors.white.withOpacity(.8),
+                  borderRadius: isSettingExpanded
+                      ? BorderRadius.circular(10)
+                      : BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10,
+                      spreadRadius: 0,
+                      color: Colors.black26,
+                    ),
+                  ],
+                ),
                 child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
                     Row(
                       mainAxisSize: MainAxisSize.max,
@@ -53,6 +80,7 @@ class _SearchPageState extends State<SearchPage> {
                         SizedBox(width: 10),
                         Expanded(
                           child: TextFormField(
+                            controller: _searchController,
                             decoration: InputDecoration.collapsed(
                               hintText: AppLocalizations.of(context)
                                   .translate("find_something"),
