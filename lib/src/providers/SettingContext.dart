dcb489e74f0d5567041efeb2a340d6da87a7c9b8
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2020-10-01 23:10:13 +0300

    create setter for languageCode

--- lib/src/providers/SettingContext.dart
+++ lib/src/providers/SettingContext.dart
@@ -2,20 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingContext extends ChangeNotifier {
-  String languageCode;
-  Future initialized;
+  String _languageCode;
+  Future<void> initialized;
+
+  String get languageCode => _languageCode;
+
+  set languageCode(String value) {
+    _languageCode = value;
+    notifyListeners();
+  }
 
   SettingContext() {
     initialized = _loadCurrentUserSettings();
   }
 
-  Future _loadCurrentUserSettings() async {
+  Future<void> _loadCurrentUserSettings() async {
     SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
     if (sharedPrefs.containsKey('languageCode')) {
       languageCode = sharedPrefs.getString('languageCode');
-      notifyListeners();
     } else {
-      sharedPrefs.setString(
+      await sharedPrefs.setString(
         'languageCode',
         languageCode,
       );
