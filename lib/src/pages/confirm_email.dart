d5b8b94e5683632957f1ac22a881ac734a0ec8d1
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2020-10-04 00:41:43 +0300

    update ConfirmEmailPage

--- lib/src/pages/confirm_email.dart
+++ lib/src/pages/confirm_email.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:menu_advisor/src/constants/colors.dart';
 import 'package:menu_advisor/src/utils/AppLocalization.dart';
 
 class ConfirmEmailPage extends StatefulWidget {
+  final String email;
+  final String id;
+
+  ConfirmEmailPage({
+    Key key,
+    @required this.id,
+    @required this.email,
+  }) : super(key: key);
+
   @override
   _ConfirmEmailPageState createState() => _ConfirmEmailPageState();
 }
@@ -15,12 +25,49 @@ class _ConfirmEmailPageState extends State<ConfirmEmailPage> {
           width: double.infinity,
           child: Column(
             mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Text(
+                  AppLocalizations.of(context)
+                      .translate("enter_code")
+                      .replaceAll('*', widget.email),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                  ),
+                ),
+              ),
               Text(
-                "${AppLocalizations.of(context).translate("enter_code")}",
+                AppLocalizations.of(context).translate("email_not_yours"),
+                textAlign: TextAlign.center,
                 style: TextStyle(
+                  color: CRIMSON,
                   fontWeight: FontWeight.bold,
-                  fontSize: 22,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [],
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: RaisedButton(
+                  onPressed: _submitForm,
+                  child: Text(
+                    AppLocalizations.of(context).translate('next'),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                    ),
+                  ),
                 ),
               ),
             ],
@@ -29,4 +76,6 @@ class _ConfirmEmailPageState extends State<ConfirmEmailPage> {
       ),
     );
   }
+
+  void _submitForm() {}
 }
