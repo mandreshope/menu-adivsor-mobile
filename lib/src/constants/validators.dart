597c39c57797164fdf6ca09c594de0e2ca902a38
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2020-10-10 06:43:54 +0300

    create constant validator

---
+++ lib/src/constants/validators.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:menu_advisor/src/utils/AppLocalization.dart';
+
+class Validators {
+  Validators._privateConstructor();
+
+  static String Function(String) required(BuildContext context) =>
+      (String value) {
+        if (value.isEmpty)
+          return AppLocalizations.of(context)
+              .translate('field_must_not_be_blank');
+
+        return null;
+      };
+}
