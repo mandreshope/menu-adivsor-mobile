5f4cc35e1ad67f35d6e93986918e28dc20d0374d
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2020-10-11 09:35:24 +0300

    update restaurant view

--- lib/src/pages/restaurant.dart
+++ lib/src/pages/restaurant.dart
@@ -3,9 +3,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:menu_advisor/src/components/cards.dart';
+import 'package:menu_advisor/src/components/dialogs.dart';
+import 'package:menu_advisor/src/constants/colors.dart';
 import 'package:menu_advisor/src/models.dart';
 import 'package:menu_advisor/src/providers/AuthContext.dart';
+import 'package:menu_advisor/src/providers/SettingContext.dart';
 import 'package:menu_advisor/src/services/api.dart';
 import 'package:menu_advisor/src/types.dart';
 import 'package:menu_advisor/src/utils/AppLocalization.dart';
@@ -25,19 +27,24 @@ class RestaurantPage extends StatefulWidget {
 
 class _RestaurantPageState extends State<RestaurantPage>
     with SingleTickerProviderStateMixin {
-  bool isInFavorite = false;
-  bool showFavoriteButton = true;
-  bool loading = false;
+  bool _isInFavorite = false;
+  bool _showFavoriteButton = true;
+  bool _loading = false;
   TabController _controller;
+  int _activeTabIndex = 0;
 
   String _searchValue = '';
-  List<SearchResult> searchResults = [];
+  List<SearchResult> _searchResults = [];
   Api _api = Api.instance;
   Timer _timer;
 
+  bool _isSearching = false;
+
+  String _lang;
+
   void _onChanged(String value) {
     setState(() {
-      loading = true;
+      _loading = true;
       _searchValue = value;
     });
 
@@ -52,18 +59,20 @@ class _RestaurantPageState extends State<RestaurantPage>
   void initState() {
     super.initState();
 
+    _lang = Provider.of<SettingContext>(context, listen: false).languageCode;
+
     _controller = TabController(
       vsync: this,
       initialIndex: 0,
-      length: 2,
+      length: 3,
     );
 
     AuthContext authContext = Provider.of<AuthContext>(context, listen: false);
-    if (authContext.currentUser == null) showFavoriteButton = false;
+    if (authContext.currentUser == null) _showFavoriteButton = false;
     if (authContext.currentUser != null &&
         authContext.currentUser.favoriteRestaurants
                 ?.indexWhere((element) => element.id == widget.restaurant.id) >
-            1) isInFavorite = true;
+            1) _isInFavorite = true;
   }
 
   Future _initSearch() async {
@@ -75,7 +84,7 @@ class _RestaurantPageState extends State<RestaurantPage>
     if (!mounted) return;
 
     setState(() {
-      loading = true;
+      _loading = true;
     });
     try {
       var results = await _api.search(
@@ -85,7 +94,7 @@ class _RestaurantPageState extends State<RestaurantPage>
         },
       );
       setState(() {
-        searchResults = results;
+        _searchResults = results;
       });
     } catch (error) {
       print(error.toString());
@@ -94,7 +103,7 @@ class _RestaurantPageState extends State<RestaurantPage>
       );
     } finally {
       setState(() {
-        loading = false;
+        _loading = false;
       });
     }
   }
@@ -102,175 +111,270 @@ class _RestaurantPageState extends State<RestaurantPage>
   _toggleFavorite() {
     Fluttertoast.showToast(
       msg: AppLocalizations.of(context).translate(
-        !isInFavorite ? 'added_to_favorite' : 'removed_from_favorite',
+        !_isInFavorite ? 'added_to_favorite' : 'removed_from_favorite',
       ),
     );
     setState(() {
-      isInFavorite = !isInFavorite;
+      _isInFavorite = !_isInFavorite;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: CustomScrollView(
-          physics: BouncingScrollPhysics(),
-          slivers: [
-            SliverAppBar(
-              expandedHeight: 200.0,
-              floating: false,
-              pinned: true,
-              stretch: true,
-              title: Text(
-                widget.restaurant.name,
-              ),
-              bottom: TabBar(
-                controller: _controller,
-                tabs: [
-                  Tab(
-                    text: AppLocalizations.of(context).translate('menus'),
-                    icon: FaIcon(
-                      FontAwesomeIcons.hamburger,
-                    ),
-                  ),
-                  Tab(
-                    text: AppLocalizations.of(context).translate('research'),
-                    icon: FaIcon(
-                      FontAwesomeIcons.search,
-                    ),
-                  ),
-                ],
-              ),
-              onStretchTrigger: () async {
-                print("Danze");
-                return;
-              },
-              actions: [
-                if (showFavoriteButton)
-                  IconButton(
-                    onPressed: _toggleFavorite,
-                    icon: Icon(
-                      isInFavorite ? Icons.favorite : Icons.favorite_border,
-                      color: Colors.white,
-                    ),
-                  ),
-              ],
-              flexibleSpace: FlexibleSpaceBar(
-                background: Image.network(
-                  widget.restaurant.imageURL,
-                  fit: BoxFit.cover,
-                  color: Colors.black45,
-                  colorBlendMode: BlendMode.darken,
-                ),
-                stretchModes: [
-                  StretchMode.zoomBackground,
-                ],
-              ),
-            ),
-            SliverFillRemaining(
-              fillOverscroll: true,
-              child: TabBarView(
-                controller: _controller,
-                physics: BouncingScrollPhysics(),
-                children: [
-                  widget.restaurant.menus.length > 0
-                      ? Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: widget.restaurant.menus
-                              .map(
-                                (e) => MenuCard(
-                                  menu: e,
-                                ),
-                              )
-                              .toList(),
-                        )
-                      : Column(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.warning,
-                              size: 80,
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 40.0,
-                              ),
-                              child: Text(
-                                AppLocalizations.of(context)
-                                    .translate('no_menu'),
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 22,
-                                  letterSpacing: 1.2,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                  Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      Column(
-                        children: [],
-                      ),
-                      Positioned(
-                        top: 20,
-                        left: 20,
-                        right: 20,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 15,
+    return WillPopScope(
+      onWillPop: () async {
+        if (_isSearching) {
+          setState(() {
+            _isSearching = false;
+          });
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (_) => BagModal(),
+              backgroundColor: Colors.transparent,
+            );
+          },
+          child: FaIcon(
+            FontAwesomeIcons.shoppingBag,
+            color: Colors.white,
+          ),
+        ),
+        appBar: _isSearching
+            ? AppBar(
+                title: Text(widget.restaurant.name),
+              )
+            : null,
+        body: SafeArea(
+          child: _isSearching
+              ? Container()
+              : CustomScrollView(
+                  physics: BouncingScrollPhysics(),
+                  slivers: [
+                    SliverAppBar(
+                      expandedHeight: 160.0,
+                      floating: false,
+                      pinned: true,
+                      stretch: true,
+                      title: Column(
+                        children: [
+                          Text(
+                            widget.restaurant.name,
                           ),
-                          decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(.8),
-                            borderRadius: BorderRadius.circular(50),
-                            boxShadow: [
-                              BoxShadow(
-                                blurRadius: 10,
-                                spreadRadius: 0,
-                                color: Colors.black26,
-                              ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.location_on),
+                              SizedBox(width: 5),
+                              Text(widget.restaurant.address),
                             ],
                           ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
+                        ],
+                      ),
+                      bottom: TabBar(
+                        controller: _controller,
+                        tabs: [
+                          Tab(
+                            text: AppLocalizations.of(context)
+                                .translate('a_la_carte'),
+                            icon: FaIcon(
+                              FontAwesomeIcons.list,
+                            ),
+                          ),
+                          Tab(
+                            text:
+                                AppLocalizations.of(context).translate('menus'),
+                            icon: FaIcon(
+                              FontAwesomeIcons.hamburger,
+                            ),
+                          ),
+                          Tab(
+                            text: AppLocalizations.of(context)
+                                .translate('drinks'),
+                            icon: FaIcon(
+                              FontAwesomeIcons.wineGlass,
+                            ),
+                          ),
+                        ],
+                      ),
+                      actions: [
+                        if (_showFavoriteButton)
+                          IconButton(
+                            onPressed: _toggleFavorite,
+                            icon: Icon(
+                              _isInFavorite
+                                  ? Icons.favorite
+                                  : Icons.favorite_border,
+                              color: Colors.white,
+                            ),
+                          ),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _isSearching = true;
+                            });
+                          },
+                          icon: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            String lang = await showDialog<String>(
+                              context: context,
+                              builder: (_) => LanguageDialog(lang: _lang),
+                            );
+                            print('Lang: $lang');
+                          },
+                          icon: Icon(
+                            Icons.language,
+                          ),
+                        ),
+                      ],
+                      flexibleSpace: FlexibleSpaceBar(
+                        background: Image.network(
+                          widget.restaurant.imageURL,
+                          fit: BoxFit.cover,
+                          color: Colors.black45,
+                          colorBlendMode: BlendMode.darken,
+                        ),
+                        stretchModes: [
+                          StretchMode.zoomBackground,
+                        ],
+                      ),
+                    ),
+                    SliverFillRemaining(
+                      fillOverscroll: true,
+                      child: TabBarView(
+                        controller: _controller,
+                        physics: BouncingScrollPhysics(),
+                        children: [
+                          Stack(
+                            fit: StackFit.expand,
                             children: [
-                              Row(
-                                mainAxisSize: MainAxisSize.max,
-                                children: [
-                                  FaIcon(
-                                    FontAwesomeIcons.search,
-                                    size: 16,
-                                  ),
-                                  SizedBox(width: 10),
-                                  Expanded(
-                                    child: TextFormField(
-                                      decoration: InputDecoration.collapsed(
-                                        hintText: AppLocalizations.of(context)
-                                            .translate("find_something"),
-                                      ),
-                                      onChanged: _onChanged,
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  top: 50,
+                                  left: 20,
+                                  right: 20,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: BACKGROUND_COLOR,
+                                  border: Border(
+                                    top: BorderSide(
+                                      color: Colors.grey[400],
+                                      style: BorderStyle.solid,
+                                      width: 1,
                                     ),
                                   ),
-                                ],
+                                ),
+                                child: SingleChildScrollView(
+                                  physics: BouncingScrollPhysics(),
+                                  child: Column(),
+                                ),
+                              ),
+                              Positioned(
+                                top: 10,
+                                left: 0,
+                                right: 0,
+                                child: Container(
+                                  height: 41,
+                                  width: double.infinity,
+                                  alignment: Alignment.center,
+                                  child: ListView(
+                                    shrinkWrap: true,
+                                    scrollDirection: Axis.horizontal,
+                                    children: [
+                                      tab(
+                                        text: "Entrées",
+                                        index: 0,
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      tab(
+                                        text: "Entrées",
+                                        index: 1,
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      tab(
+                                        text: "Entrées",
+                                        index: 2,
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
                             ],
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                ],
+                          Column(),
+                          Column(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+
+  Widget tab({
+    String text,
+    int index,
+  }) {
+    return Material(
+      child: InkWell(
+        onTap: () {
+          setState(() {
+            _activeTabIndex = index;
+          });
+        },
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            text,
+          ),
+          decoration: BoxDecoration(
+            color: BACKGROUND_COLOR,
+            border: Border(
+              top: BorderSide(
+                width: 1,
+                style: BorderStyle.solid,
+                color: Colors.grey[400],
               ),
+              right: BorderSide(
+                width: 1,
+                style: BorderStyle.solid,
+                color: Colors.grey[400],
+              ),
+              left: BorderSide(
+                width: 1,
+                style: BorderStyle.solid,
+                color: Colors.grey[400],
+              ),
+              bottom: _activeTabIndex == index
+                  ? BorderSide(
+                      width: 1,
+                      style: BorderStyle.solid,
+                      color: BACKGROUND_COLOR,
+                    )
+                  : BorderSide(
+                      width: 1,
+                      style: BorderStyle.solid,
+                      color: Colors.grey[400],
+                    ),
             ),
-          ],
+          ),
         ),
       ),
     );
