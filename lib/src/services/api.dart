f7c7b6e1111498bf36cf232efba77a144aaae367
Hajanirina Ridjvan Randrianandraina <darijavan@gmail.com>
2020-10-29 11:38:09 +0300

    add getCommandOfUser method

--- lib/src/services/api.dart
+++ lib/src/services/api.dart
@@ -490,6 +490,22 @@ class Api {
     );
   }
 
+  Future<List<Command>> getCommandOfUser(User user) async {
+    await _refreshTokens();
+
+    return http.get('$_apiURL/commands?filter={"relatedUser": "${user.id}"}', headers: {
+      'authorization': 'Bearer $_accessToken',
+      'Content-Type': 'application/json',
+    }).then<List<Command>>((response) {
+      if (response.statusCode == 200) {
+        List datas = jsonDecode(response.body);
+        return datas.map<Command>((data) => Command.fromJson(data)).toList();
+      }
+
+      return Future.error(jsonDecode(response.body));
+    });
+  }
+
   Future<Map> sendCommand({
     String relatedUser,
     String commandType,
