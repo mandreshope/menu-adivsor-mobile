957756e48fd831d8899989f070dc5912169ea747
aina_sitraka <ainastrk@gmail.com>
2021-01-04 17:14:51 +0300

    google maps integration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAUqHj0R8M8Oslaz_vtaANY-cCEPmpV7c8") //google maps api key
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/src/pages/map.dart
+++ lib/src/pages/map.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map/plugin_api.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:latlong/latlong.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:menu_advisor/src/components/dialogs.dart';
 import 'package:menu_advisor/src/constants/colors.dart';
 import 'package:menu_advisor/src/models.dart';
@@ -28,7 +26,7 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   Position _currentPosition;
-  MapController _mapController = MapControllerImpl();
+  // MapController _mapController = MapControllerImpl();
   String _searchValue = '';
   Timer _updateLocationInterval;
   Timer _timer;
@@ -36,6 +34,71 @@ class _MapPageState extends State<MapPage> {
   Map<String, dynamic> filters = Map();
   bool _loading = false;
   Api _api = Api.instance;
+
+  //google maps 
+  Completer<GoogleMapController> _mapController = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final _center = LatLng(37.43296265331129, -122.08832357078792);
+
+  final Set<Marker> _markers = {};
+  
+  LatLng _lastMapPosition = _center;
+
+  void _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  Future<void> addMyPosMarker() async {
+    setState(() {
+      _markers.add(Marker(
+        // This marker id can be anything that uniquely identifies each marker.
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: LatLng(_currentPosition.latitude, _currentPosition.longitude,),
+        infoWindow: InfoWindow(
+          title: AppLocalizations.of(context).translate('you'),
+        ),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+      ));
+    });
+    // var map = await _mapController.future;
+    // map.animateCamera(
+    //   CameraUpdate.newCameraPosition(
+    //     CameraPosition( target: LatLng(
+    //       _currentPosition.latitude,
+    //       _currentPosition.longitude,
+    //     ), zoom: 15)
+    //   ))
+    // .catchError((onError) {
+
+    // });
+  }
+
+  void addAllRestaurantsMarkers() {
+    setState(() {
+      for (var restaurant in _nearestRestaurants) {
+        String markerId = restaurant.content['location']['coordinates'][1].toString()+restaurant.content['location']['coordinates'][0].toString();
+        _markers.add(Marker(
+          // This marker id can be anything that uniquely identifies each marker.
+          markerId: MarkerId(markerId),
+          position: LatLng(
+            restaurant.content['location']['coordinates'][1],
+            restaurant.content['location']['coordinates'][0],
+          ),
+          infoWindow: InfoWindow(
+            title: restaurant.content['name'],
+          ),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+        ));
+      }
+    });
+  }
+
+  // google maps
 
   @override
   void initState() {
@@ -64,6 +127,9 @@ class _MapPageState extends State<MapPage> {
     setState(() {
       _currentPosition = position;
     });
+
+    addMyPosMarker();
+    addAllRestaurantsMarkers();
   }
 
   void _checkForLocationPermission() async {
@@ -156,89 +222,99 @@ class _MapPageState extends State<MapPage> {
           fit: StackFit.expand,
           children: [
             _currentPosition != null
-                ? FlutterMap(
-                    mapController: _mapController,
-                    options: MapOptions(
-                      minZoom: 1.0,
-                      maxZoom: 18.0,
-                      zoom: 15.0,
-                      interactive: true,
-                      center: LatLng(
-                        _currentPosition.latitude,
-                        _currentPosition.longitude,
-                      ),
-                    ),
-                    layers: [
-                      TileLayerOptions(
-                        urlTemplate:
-                            'https://api.mapbox.com/styles/v1/darijavan/ckg69xpmk4eft19qhv1dhpe9k/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGFyaWphdmFuIiwiYSI6ImNqb3diNXZ0eDBxMjkzdW9kc2F3aHh6M2EifQ.gTXds1mQoGDFQ5bhIeYvqA',
-                        subdomains: ['a', 'b', 'c'],
-                      ),
-                      MarkerLayerOptions(
-                        markers: [
-                          ..._nearestRestaurants
-                              .map(
-                                (restaurant) => Marker(
-                                  width: 80,
-                                  height: 60,
-                                  point: LatLng(
-                                    restaurant.content['location']['coordinates'][1],
-                                    restaurant.content['location']['coordinates'][0],
-                                  ),
-                                  builder: (BuildContext context) => Container(
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                        TextTranslator(
-                                          restaurant.content['name'],
-                                        ),
-                                        SizedBox(
-                                          height: 5,
-                                        ),
-                                        FaIcon(
-                                          FontAwesomeIcons.utensils,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              )
-                              .toList(),
-                          Marker(
-                            width: 100,
-                            height: 60,
-                            point: LatLng(
-                              _currentPosition.latitude,
-                              _currentPosition.longitude,
-                            ),
-                            anchorPos: AnchorPos.align(AnchorAlign.top),
-                            builder: (BuildContext context) => Container(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  TextTranslator(
-                                    AppLocalizations.of(context).translate('you'),
-                                    style: TextStyle(
-                                      color: CRIMSON,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 5,
-                                  ),
-                                  Icon(
-                                    Icons.location_on,
-                                    color: CRIMSON,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                ? GoogleMap(
+                    mapType: MapType.normal,
+                    initialCameraPosition: _kGooglePlex,
+                    zoomControlsEnabled: false,
+                    onCameraMove: _onCameraMove,
+                    markers:_markers,
+                    onMapCreated: (GoogleMapController controller) {
+                      _mapController.complete(controller);
+                    },
                   )
+                // FlutterMap(
+                //     mapController: _mapController,
+                //     options: MapOptions(
+                //       minZoom: 1.0,
+                //       maxZoom: 18.0,
+                //       zoom: 15.0,
+                //       interactive: true,
+                //       center: LatLng(
+                //         _currentPosition.latitude,
+                //         _currentPosition.longitude,
+                //       ),
+                //     ),
+                //     layers: [
+                //       TileLayerOptions(
+                //         urlTemplate:
+                //             'https://api.mapbox.com/styles/v1/darijavan/ckg69xpmk4eft19qhv1dhpe9k/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGFyaWphdmFuIiwiYSI6ImNqb3diNXZ0eDBxMjkzdW9kc2F3aHh6M2EifQ.gTXds1mQoGDFQ5bhIeYvqA',
+                //         subdomains: ['a', 'b', 'c'],
+                //       ),
+                //       MarkerLayerOptions(
+                //         markers: [
+                //           ..._nearestRestaurants
+                //               .map(
+                //                 (restaurant) => Marker(
+                //                   width: 80,
+                //                   height: 60,
+                //                   point: LatLng(
+                //                     restaurant.content['location']['coordinates'][1],
+                //                     restaurant.content['location']['coordinates'][0],
+                //                   ),
+                //                   builder: (BuildContext context) => Container(
+                //                     child: Column(
+                //                       crossAxisAlignment: CrossAxisAlignment.center,
+                //                       children: [
+                //                         TextTranslator(
+                //                           restaurant.content['name'],
+                //                         ),
+                //                         SizedBox(
+                //                           height: 5,
+                //                         ),
+                //                         FaIcon(
+                //                           FontAwesomeIcons.utensils,
+                //                         ),
+                //                       ],
+                //                     ),
+                //                   ),
+                //                 ),
+                //               )
+                //               .toList(),
+                //           Marker(
+                //             width: 100,
+                //             height: 60,
+                //             point: LatLng(
+                //               _currentPosition.latitude,
+                //               _currentPosition.longitude,
+                //             ),
+                //             anchorPos: AnchorPos.align(AnchorAlign.top),
+                //             builder: (BuildContext context) => Container(
+                //               child: Column(
+                //                 crossAxisAlignment: CrossAxisAlignment.center,
+                //                 children: [
+                //                   TextTranslator(
+                //                     AppLocalizations.of(context).translate('you'),
+                //                     style: TextStyle(
+                //                       color: CRIMSON,
+                //                       fontWeight: FontWeight.bold,
+                //                       fontSize: 20,
+                //                     ),
+                //                   ),
+                //                   SizedBox(
+                //                     height: 5,
+                //                   ),
+                //                   Icon(
+                //                     Icons.location_on,
+                //                     color: CRIMSON,
+                //                   ),
+                //                 ],
+                //               ),
+                //             ),
+                //           ),
+                //         ],
+                //       ),
+                //     ],
+                //   )
                 : Center(
                     child: CircularProgressIndicator(),
                   ),
@@ -380,14 +456,25 @@ class _MapPageState extends State<MapPage> {
                                                           maxHeight: 26,
                                                           maxWidth: 26,
                                                         ),
-                                                        onPressed: () {
-                                                          _mapController.move(
-                                                            LatLng(
-                                                              restaurant.location.coordinates[0],
-                                                              restaurant.location.coordinates[1],
-                                                            ),
-                                                            15,
-                                                          );
+                                                        onPressed: () async {
+                                                          var map = await _mapController.future;
+                                                          map.animateCamera(
+                                                            CameraUpdate.newCameraPosition(
+                                                              CameraPosition( target: LatLng(
+                                                                restaurant.location.coordinates[1],
+                                                                restaurant.location.coordinates[0],
+                                                              ), zoom: 15)
+                                                            ))
+                                                          .catchError((onError) {
+
+                                                          });
+                                                          // _mapController.move(
+                                                          //   LatLng(
+                                                          //     restaurant.location.coordinates[0],
+                                                          //     restaurant.location.coordinates[1],
+                                                          //   ),
+                                                          //   15,
+                                                          // );
                                                         },
                                                       ),
                                                     ],
@@ -417,13 +504,21 @@ class _MapPageState extends State<MapPage> {
               child: FloatingActionButton(
                 onPressed: () async {
                   _updateLocation(null);
-                  _mapController.move(
-                    LatLng(
-                      _currentPosition.latitude,
-                      _currentPosition.longitude,
-                    ),
-                    15,
-                  );
+                  var map = await _mapController.future;
+                  map.animateCamera(
+                    CameraUpdate.newCameraPosition(
+                      CameraPosition( target: LatLng(_currentPosition.latitude,_currentPosition.longitude),zoom: 15))
+                    )
+                  .catchError((onError) {
+
+                  });
+                  // _mapController.move(
+                  //   LatLng(
+                  //     _currentPosition.latitude,
+                  //     _currentPosition.longitude,
+                  //   ),
+                  //   15,
+                  // );
                 },
                 child: Icon(Icons.my_location),
               ),
